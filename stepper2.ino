2172856d0183a8403570b8d95111294e802a8db0
valera <rmnvalera@gmail.com>
2019-06-16 22:03:46 +0300

    final v1.0

--- stepper2.ino
+++ stepper2.ino
@@ -35,6 +35,7 @@ String command;
 
 void setup(){
   Serial.begin(9600);
+  
   Serial.println("Start program ...");
   
   servo.attach(SERVOPIN);
@@ -53,81 +54,107 @@ void loop(){
   }
 }
 
+void CommandHandler(String cmd){
 
-void CommandHandler(String Str){
-
-switch(Str[0]){
+switch(cmd[0]){
   case 's':
     Serial.println("Command Servo");
-    switch(Str[1]){
+    switch(cmd[1]){
       case 'o':
         Serial.println("Command mode oopen for command Servo");
               for(angle = 0; angle < 180; angle++){
                 servo.write(angle);
               }
         break;
+        
       case 'c':
         Serial.println("Command mode close for command Servo");
               for(angle = 180; angle > 0; angle--){
                 servo.write(angle);
               }
         break;
+        
       default:
         Serial.println("Unknown command mode for command Servo");
     }
     break;
+    
   case 'm':
     Serial.println("Command motor");
-    switch(Str[1]){
+    switch(cmd[1]){
       case 't':
-      Direction = true;
-      stepMotorCommand(Str);
-      break;
+        Direction = true;
+        stepMotorCommand(cmd);
+        break;
+      
       case 'f':
-      Direction = false;
-      stepMotorCommand(Str);
-      break;
+        Direction = false;
+        stepMotorCommand(cmd);
+        break;
+        
+      default:
+        Serial.println("Unknown command mode for command Motor");
     }
     break;
+    
   default:
   Serial.println("Unknown command");
 }
 }
 
+void stepMotorCommand(String cmdMotor){
+  int stepsMotor = readStep(cmdMotor);
+    switch(cmdMotor[2]){
+      case '1':
+        Serial.println("Motor 1");
+        setupMotors(1, stepsMotor);
+        break;
+      case '2':
+        Serial.println("Motor 2");
+        setupMotors(2, stepsMotor);
+        break;
+      case '3':
+      Serial.println("Motor 3");
+        setupMotors(3, stepsMotor);
+        break;
+      default:
+        Serial.println("Unknown command mode for command Motor");
+  }
+  steps_left = MOTOR_STEP;
+}
 
-void stepMotorCommand(String Str){
-        switch(Str[2]){
-        case '1':
-          Serial.println("Motor 1");
-          while(steps_left>0){
-            currentMillis = micros();
-            if(currentMillis-last_time>=1000){
-              stepper(1, M1IN1, M1IN2, M1IN3, M1IN4);
-              time=time+micros()-last_time;
-              last_time=micros();
-            steps_left--;
-            }
-          }
-          break;
-        case '2':
-          Serial.println("Motor 2");
-          break;
-        case '3':
-        steps_left = 2000;
-          while(steps_left>0){
-            currentMillis = micros();
-            if(currentMillis-last_time>=1000){
-              stepper(1, M3IN1, M3IN2, M3IN3, M3IN4);
-              time=time+micros()-last_time;
-              last_time=micros();
-            steps_left--;
-            }
-          }
-          break;
-        default:
-          Serial.println("Unknown command mode for command Motor");
-      }
-      steps_left = MOTOR_STEP;
+void setupMotors(int namberMotor, int steps_left){
+  if (steps_left == 0){
+    Serial.println("stepsMotor = 0");
+    return; 
+  }
+  
+  if(namberMotor == 1){
+    controlMotor(steps_left, M1IN1, M1IN2, M1IN3, M1IN4);
+  }
+  if(namberMotor == 2){
+    controlMotor(steps_left, M2IN1, M2IN2, M2IN3, M2IN4);
+  }
+  if(namberMotor == 3){
+    controlMotor(steps_left, M3IN1, M3IN2, M3IN3, M3IN4);
+  }
+}
+
+void controlMotor(int steps_left, int IN1, int IN2, int IN3, int IN4){
+  while(steps_left>0){
+    currentMillis = micros();
+    if(currentMillis-last_time>=1000){
+      stepper(1, IN1, IN2, IN3, IN4);
+      time=time+micros()-last_time;
+      last_time=micros();
+      steps_left--;
+    }
+  }
+}
+
+int readStep(String cmd){  
+  String stringStep = cmd.substring(3);
+  return stringStep.toInt();  
 }
 
 void initStepMotor(int IN1, int IN2, int IN3, int IN4){
